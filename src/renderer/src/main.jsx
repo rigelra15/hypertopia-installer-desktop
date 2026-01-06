f9cfc18d9fc4017b3f6b445a6171b6db12729dc1
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2026-01-06 15:37:01 +0700

    feat: integrate AuthProvider in app root
    - Wrap app with AuthProvider context
    - Enable authentication state across all components

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -4,6 +4,7 @@ import './utils/loadIcons' // Load icons for offline use
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { AuthProvider } from './contexts/AuthContext'
 import { LanguageProvider } from './contexts/LanguageContext'
 import { ThemeProvider } from './contexts/ThemeContext'
 
@@ -13,9 +14,11 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <LanguageProvider>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
+        <AuthProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </AuthProvider>
       </LanguageProvider>
     </ThemeProvider>
   </StrictMode>
