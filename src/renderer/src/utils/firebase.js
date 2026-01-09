90ec89e433667fccf730fb0ecbf8227111d50ea9
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2026-01-09 22:20:39 +0700

    fix(firebase): update storage config to correct project
    - Switch from hypertopia-id-bc to hypertopia-id-sec project
    - Use same Firebase Storage config as website for consistency
    - Update all Firebase project identifiers
    - Maintain free tier compatibility

--- src/renderer/src/utils/firebase.js
+++ src/renderer/src/utils/firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCDfNWLyB_sU4TZk4s-TrC27qXZgtMPJZQ',
-  authDomain: 'hypertopia-id-bc.firebaseapp.com',
-  databaseURL: 'https://hypertopia-id-bc-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'hypertopia-id-bc',
-  storageBucket: 'hypertopia-id-bc.appspot.com',
-  messagingSenderId: '1089206526652',
-  appId: '1:1089206526652:web:fbeee5a8bc3c1c0478e6f0'
+// Firebase config untuk Storage (project lama - masih free, sama seperti website)
+const firebaseStorageConfig = {
+  apiKey: 'AIzaSyB1V-EMM1yaYm6qEge6KYbL9lJ9HZOl93k',
+  authDomain: 'hypertopia-id-sec.firebaseapp.com',
+  databaseURL: 'https://hypertopia-id-sec-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'hypertopia-id-sec',
+  storageBucket: 'hypertopia-id-sec.appspot.com',
+  messagingSenderId: '471391816995',
+  appId: '1:471391816995:web:1174a390e903778d5d5096'
 }
 
-const app = initializeApp(firebaseConfig, 'hypertopia-storage')
+const app = initializeApp(firebaseStorageConfig, 'hypertopia-storage')
 export const storage = getStorage(app)
